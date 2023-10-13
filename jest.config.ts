cb9359d93852f146fdbc44dadc47e007bd0bf2a0
janine65 <janine.franken@gmail.com>
2023-10-13 21:16:47 +0200

    Initial

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,22 @@
-import { getJestProjects } from '@nx/jest';
-
+/* eslint-disable */
 export default {
-  projects: getJestProjects(),
+  displayName: 'keller-frontend',
+  preset: './jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../coverage/apps/keller-frontend',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
 };
